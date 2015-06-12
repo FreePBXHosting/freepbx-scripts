59a890e48ef682c585707d898537b9567c640d7c
ajleach <ajleach@gmail.com>
2015-06-12 13:19:17 -0500

    update if statements

--- disableisymphony/disableisymphony.sh
+++ disableisymphony/disableisymphony.sh
@@ -2,17 +2,17 @@
 #############################################
 # Created by Alex Leach - @ajleach          #
 # FreePBXHosting.com - @freepbxhosting      #
-# VERSION 1.0  RELEASE DATE JUN 12 2015     #
+# VERSION 1.1  RELEASE DATE JUN 12 2015     #
 # DESC: DISABLES ISYMPHONY IF INSTALLED     #
 #############################################
 
 echo ""
-if [rpm -qa | egrep -q iSymphonyServerV3]
+if [ rpm -qa | egrep -q iSymphonyServerV3 ]
 then
 	service iSymphonyServerV3 stop
 	chkconfig iSymphonyServerV3 off
 	echo -e "iSymphonyServerV3 has been disabled.\n"
-elif [rpm -qa | egrep -q iSymphonyServer]
+elif [ rpm -qa | egrep -q iSymphonyServer ]
 then
 	service iSymphonyServer stop
 	chkconfig iSymphonyServer off

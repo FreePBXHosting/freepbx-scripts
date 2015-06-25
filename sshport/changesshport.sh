ad318cd659741acafe629425266d56e7cc966524
ajleach <ajleach@gmail.com>
2015-06-25 15:06:21 -0500

    add prompt after error

--- sshport/changesshport.sh
+++ sshport/changesshport.sh
@@ -9,7 +9,6 @@
 echo ""
 echo -n "Please enter the port you would like SSH to run on > "
 while read SSHPORT; do
-	echo -n "Please enter the port you would like SSH to run on > "
 	if [[ $port =~ ^[0-9]{2,5}$ ]]; then
 		# Create backup of current SSH config
 		NOW=$(date +"%m_%d_%Y-%H_%M_%S")
@@ -25,6 +24,7 @@ while read SSHPORT; do
 		exit 0
 	else
 		echo -e "Invalid port: must be numeric!"
+		echo -n "Please enter the port you would like SSH to run on > "
 	fi
 done
 

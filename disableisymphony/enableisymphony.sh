716705d04bbe0d70b7ee0fc5000943c7a80257f5
ajleach <ajleach@gmail.com>
2015-07-08 11:41:08 -0500

    check to see if isymphony is already enabled

--- disableisymphony/enableisymphony.sh
+++ disableisymphony/enableisymphony.sh
@@ -2,7 +2,7 @@
 #############################################
 # Created by Alex Leach - @ajleach          #
 # FreePBXHosting.com - @freepbxhosting      #
-# VERSION 1.0  RELEASE DATE JUN 12 2015     #
+# VERSION 1.3  RELEASE DATE JUN 12 2015     #
 # DESC: ENABLES ISYMPHONY IF INSTALLED      #
 #############################################
 
@@ -10,14 +10,24 @@ echo ""
 
 if rpm -qa | egrep -q iSymphonyServerV3;
 then
-	service iSymphonyServerV3 start &> /dev/null
-	chkconfig iSymphonyServerV3 on
-	echo -e "iSymphonyServerV3 has been enabled.\n"
+	if chkconfig iSymphonyServerV3;
+	then
+		echo -e "iSymphony is already enabled.\n"
+		exit 0
+	else
+		service iSymphonyServerV3 start &> /dev/null
+		chkconfig iSymphonyServerV3 on
+		echo -e "iSymphonyServerV3 has been enabled.\n"
 elif rpm -qa | egrep -q iSymphonyServer;
 then
-	service iSymphonyServer start &> /dev/null
-	chkconfig iSymphonyServer on
-	echo -e "iSymphonyServer has been enabled.\n"
+	if chkconfig iSymphonyServer;
+	then
+		echo -e "iSymphony is already enabled.\n"
+		exit 0
+	else
+		service iSymphonyServer start &> /dev/null
+		chkconfig iSymphonyServer on
+		echo -e "iSymphonyServer has been enabled.\n"
 else
 	echo -e "iSymphony does not appear to be installed."
 fi

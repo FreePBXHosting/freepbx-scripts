310e68c48597ac6c7aefa1c8c4ce0601724ebaad
ajleach <ajleach@gmail.com>
2015-07-08 11:45:02 -0500

    check to see if isymphony is already disabled

--- disableisymphony/disableisymphony.sh
+++ disableisymphony/disableisymphony.sh
@@ -2,21 +2,33 @@
 #############################################
 # Created by Alex Leach - @ajleach          #
 # FreePBXHosting.com - @freepbxhosting      #
-# VERSION 1.2  RELEASE DATE JUN 12 2015     #
+# VERSION 1.3  RELEASE DATE JUN 12 2015     #
 # DESC: DISABLES ISYMPHONY IF INSTALLED     #
 #############################################
 
 echo ""
 if rpm -qa | egrep -q iSymphonyServerV3;
 then
-	service iSymphonyServerV3 stop &> /dev/null
-	chkconfig iSymphonyServerV3 off
-	echo -e "iSymphonyServerV3 has been disabled.\n"
+	if chkconfig iSymphonyServerV3;
+	then
+		service iSymphonyServerV3 stop &> /dev/null
+		chkconfig iSymphonyServerV3 off
+		echo -e "iSymphonyServerV3 has been disabled.\n"
+	else
+		echo -e "iSymphonyServerV3 is already disabled.\n"
+		service iSymphonyServerV3 stop &> /dev/null
+		exit 0
 elif rpm -qa | egrep -q iSymphonyServer;
 then
-	service iSymphonyServer stop &> /dev/null
-	chkconfig iSymphonyServer off
-	echo -e "iSymphonyServer has been disabled.\n"
+	if chkconfig iSymphonyServer;
+	then
+		service iSymphonyServer stop &> /dev/null
+		chkconfig iSymphonyServer off
+		echo -e "iSymphonyServer has been disabled.\n"
+	else
+		echo -e "iSymphonyServer is already disabled.\n"
+		service iSymphonyServer stop &> /dev/null
+		exit 0
 else
 	echo -e "iSymphony does not appear to be installed."
 fi

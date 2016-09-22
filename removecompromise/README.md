24178b10a6ca08c37c735f27ca8e5a72ed00b3d6
ajleach <ajleach@gmail.com>
2016-09-22 10:50:14 -0500

    Add removecompromise script

---
+++ removecompromise/README.md
@@ -0,0 +1,9 @@
+# Clean Up thankuohoh compromise in FreePBX
+
+This script will attempt to clean up the compromise described here:
+
+http://community.freepbx.org/t/i-ve-been-hacked-macro-thanku-outcall-thankuohoh/32897
+
+This by no means is a 'smart' script, and you will need to verify that things are cleaned up manually. Also a good idea to check /etc/passwd and /etc/shadow for rogue accounts.
+
+Basically, YMMV.
---
+++ removecompromise/removecompromise.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#############################################
+# Created by Alex Leach - @ajleach          #
+# FreePBXHosting.com - @freepbxhosting      #
+# VERSION 0.1       UPDATED SEP 22 2016     #
+# DESC: CLEANS UP thankuohoh COMPROMISE     #
+#############################################
+
+echo ""
+
+if [ ! -f /etc/schmooze/pbx-version ]; then
+	echo -e "This does not appear to be a FreePBX server. No changes will be made.\n"
+	exit 0
+fi
+
+# Create backup of current httpd.conf
+NOW=$(date +"%m_%d_%Y-%H_%M_%S")
+/bin/cp /etc/asterisk/extensions_custom.conf /etc/asterisk/extensions_custom.conf.bckup.$NOW
+
+sed -i -e '/thankuohoh/d' /etc/asterisk/extensions_custom.conf
+
+rm -rf /var/www/html/admin/libraries/php-upgrade/ext/config.php
+
+rm -rf /var/www/html/admin/modules/weather*
+
+amportal chown
+amportal a ma refreshsignatures
+amportal a r
+
+amportal a ma enable fw_ari
+amportal a ma upgradeall
+amportal a r
+
+amportal chown
+
+echo ""
+echo -e "Cleanup is done - manually verify that no other compromises exist. Also check /etc/passwd and /etc/shadow for rogue accounts."
+echo ""
